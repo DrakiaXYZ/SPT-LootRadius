eee02cc1613998db4480be4431c3638730b1af28
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2024-09-09 23:50:50 -0700

    - Use proper stash grid class ID - Add new traderController to the known ItemOwners

--- Patches/GameStartedPatch.cs
+++ Patches/GameStartedPatch.cs
@@ -27,9 +27,10 @@ namespace DrakiaXYZ.LootRadius.Patches
             {
                 // Create our fake stash, note we use "fake" here to have the label show as "LOOT"
                 _stash = Singleton<ItemFactory>.Instance.CreateFakeStash("fake");
-                StashGridClass stashGridClass = new StashGridClass(_stash.Id, 10, 10, true, false, Array.Empty<ItemFilter>(), _stash);
+                StashGridClass stashGridClass = new StashGridClass(_stash.Grid.Id, 10, 10, true, false, Array.Empty<ItemFilter>(), _stash);
                 _stash.Grids = new StashGridClass[] { stashGridClass };
                 var traderController = new TraderControllerClass(_stash, "RadiusStash", "Nearby Items", false, EOwnerType.Profile, null, null);
+                Singleton<GameWorld>.Instance.ItemOwners.Add(traderController, default(GameWorld.GStruct118));
 
                 // Destroy the loot item from the world when we take it
                 traderController.RemoveItemEvent += (GEventArgs3 args) => {
