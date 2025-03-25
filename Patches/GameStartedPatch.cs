6235c3ce60b2474cfa7c634253f0c08b0f9b8641
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2025-03-24 17:14:21 -0700

    Generate the stash ID based on the profile ID, but don't actually use the profile ID, as BSG uses it Use RegisteredPlayers to get list of players to generate stashes for

--- Patches/GameStartedPatch.cs
+++ Patches/GameStartedPatch.cs
@@ -1,10 +1,11 @@
 ﻿using SPT.Reflection.Patching;
 using EFT.InventoryLogic;
 using EFT;
-using System;
 using System.Reflection;
 using Comfort.Common;
 using DrakiaXYZ.LootRadius.Helpers;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace DrakiaXYZ.LootRadius.Patches
 {
@@ -27,23 +28,37 @@ namespace DrakiaXYZ.LootRadius.Patches
             // Setup the radius stash on raid start
             if (_stash == null)
             {
-                foreach (var player in Singleton<GameWorld>.Instance.AllAlivePlayersList)
+                foreach (var player in Singleton<GameWorld>.Instance.RegisteredPlayers)
                 {
                     if (player.IsAI) continue;
 
-                    var stash = Singleton<ItemFactoryClass>.Instance.CreateFakeStash(player.ProfileId);
+                    // We will use a stash ID generated based on the profile ID, so it's constant, but doesn't collide with BSG's uses
+                    string stashId = GetProfileStashId(player.ProfileId);
+                    var stash = Singleton<ItemFactoryClass>.Instance.CreateFakeStash(stashId);
                     var stashGridClass = new LootRadiusStashGrid("lootRadiusGrid", stash);
                     stash.Grids = new StashGridClass[] { stashGridClass };
 
                     var traderController = new TraderControllerClass(stash, LootRadiusStashGrid.GRIDID, "Nearby Items", false, EOwnerType.Profile);
                     Singleton<GameWorld>.Instance.ItemOwners.Add(traderController, default(GameWorld.GStruct126));
 
-                    if (player.PlayerId == GamePlayerOwner.MyPlayer.PlayerId)
+                    if (player.ProfileId == GamePlayerOwner.MyPlayer.ProfileId)
                     {
                         _stash = stash;
                     }
                 }
             }
+        }
+
+        private static string GetProfileStashId(string profileId)
+        {
+            byte[] encodedProfileId = Encoding.UTF8.GetBytes(profileId);
+            byte[] hashBytes = (new SHA256Managed()).ComputeHash(encodedProfileId);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                sb.Append(hashBytes[i].ToString("X2"));
+            }
+            return sb.ToString().Substring(0, 24).ToLower();
         }
     }
 }
